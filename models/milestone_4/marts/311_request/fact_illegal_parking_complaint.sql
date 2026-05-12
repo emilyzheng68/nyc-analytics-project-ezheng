3bffb4ef0a10706186040d48d423bf4a14d0156b
Emily Zheng <xyuan15@gmail.com>
2026-05-12 03:51:58 +0000

    commit

--- models/milestone_4/marts/311_request/fact_illegal_parking_complaint.sql
+++ models/milestone_4/marts/311_request/fact_illegal_parking_complaint.sql
@@ -1,14 +1,15 @@
 -- Fact_Illegal_Parking_Complaint
 WITH stg AS (
-    SELECT * FROM {{ ref('stg_nycservice_311_dot') }}
-    WHERE complaint_type LIKE '%Illegal Parking%' 
+    SELECT * 
+    FROM {{ ref('stg_nyc_311service_dot') }}        -- FIXED: Correct reference name
+    WHERE complaint_type LIKE '%Illegal Parking%'
        OR complaint_type LIKE '%Parking%'
 ),
 
 fact AS (
     SELECT
         {{ dbt_utils.generate_surrogate_key(['unique_key']) }} AS complaint_fact_key,
-        
+
         -- Foreign Keys
         {{ dbt_utils.generate_surrogate_key(['CAST(created_date AS DATE)']) }} AS date_created_key,
         {{ dbt_utils.generate_surrogate_key(['CAST(closed_date AS DATE)']) }} AS date_closed_key,
@@ -21,6 +22,7 @@ fact AS (
         TIMESTAMP_DIFF(closed_date, created_date, MINUTE) AS resolution_time_minutes,
 
         CURRENT_TIMESTAMP() AS _marts_loaded_at
+
     FROM stg
 )
 
