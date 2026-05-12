9d3489890dc6bed0723e3af4b28b49ff78932052
Emily Zheng <xyuan15@gmail.com>
2026-05-12 03:28:07 +0000

    commit

--- models/milestone_4/staging/stg_motor.sql
+++ models/milestone_4/staging/stg_motor.sql
@@ -1,6 +1,4 @@
 -- Staging: Motor Vehicle Collisions (Last 5 Years)
--- Cleaned and standardized version of raw crash data
-
 WITH source AS (
     SELECT * 
     FROM {{ source('raw', 'motorvehicle_collisions_crashes') }}
@@ -8,21 +6,18 @@ WITH source AS (
 
 cleaned AS (
     SELECT
-        -- Unique Identifier
         CAST(collision_id AS STRING) AS collision_id,
 
-        -- Dates & Time
-        CAST(crash_date AS DATE) AS crash_date,
+        -- Fix date handling
+        PARSE_DATE('%Y-%m-%d', crash_date) AS crash_date,                    -- Convert string to DATE
         PARSE_TIME('%H:%M', crash_time) AS crash_time,
-        TIMESTAMP(crash_date, PARSE_TIME('%H:%M', crash_time)) AS crash_datetime,
+        TIMESTAMP(PARSE_DATE('%Y-%m-%d', crash_date), PARSE_TIME('%H:%M', crash_time)) AS crash_datetime,
 
-        -- Location
         borough,
         zip_code,
         CAST(latitude AS FLOAT64) AS latitude,
         CAST(longitude AS FLOAT64) AS longitude,
 
-        -- Measures
         CAST(number_of_persons_injured AS INT64) AS number_of_persons_injured,
         CAST(number_of_persons_killed AS INT64) AS number_of_persons_killed,
         CAST(number_of_pedestrians_injured AS INT64) AS number_of_pedestrians_injured,
@@ -32,21 +27,19 @@ cleaned AS (
         CAST(number_of_motorist_injured AS INT64) AS number_of_motorist_injured,
         CAST(number_of_motorist_killed AS INT64) AS number_of_motorist_killed,
 
-        -- Descriptive Fields
         contributing_factor_vehicle_1,
         contributing_factor_vehicle_2,
         vehicle_type_code1,
         vehicle_type_code2,
 
-        -- Metadata
         CURRENT_TIMESTAMP() AS _stg_loaded_at
 
     FROM source
 
     WHERE collision_id IS NOT NULL
       AND crash_date IS NOT NULL
-      -- Filter to last 5 years
-      AND crash_date >= DATE_SUB(CURRENT_DATE(), INTERVAL 5 YEAR)
+      -- Filter to last 5 years (after converting to DATE)
+      AND PARSE_DATE('%Y-%m-%d', crash_date) >= DATE_SUB(CURRENT_DATE(), INTERVAL 5 YEAR)
 )
 
 SELECT * FROM cleaned
